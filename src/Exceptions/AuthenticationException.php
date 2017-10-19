7aee9eee4ff183b09263114b0c84a7d9444c98db
eric <liaogd79@gmail.com>
2017-10-19 10:37:11 +0800

    fix line separator CR to LF

--- src/Exceptions/AuthenticationException.php
+++ src/Exceptions/AuthenticationException.php
@@ -1,1 +1,19 @@
-<?phpnamespace Maras0830\BattleNetApi\Exceptions;use Exception;/** * Class AuthenticationException * @package Maras0830\BattleNetApi\Exceptions */class AuthenticationException extends Exception{    /**     *     */    public function __construct()    {        $this->message = 'Authentication failed.';    }}
+<?php
+namespace Maras0830\BattleNetApi\Exceptions;
+
+use Exception;
+
+/**
+ * Class AuthenticationException
+ * @package Maras0830\BattleNetApi\Exceptions
+ */
+class AuthenticationException extends Exception
+{
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->message = 'Authentication failed.';
+    }
+}
